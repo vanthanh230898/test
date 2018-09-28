8b29e7c8b7cc7946fad20fb6729079358102ee37
PhamTuanHung <43666819+PhamTuanHung@users.noreply.github.com>
2018-09-28 15:59:07 +0700

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,15 @@
+/*
+In chu Hello ra man hinh
+Name: Nguyen Duc Thinh
+Date: 26/09/2018
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello Duc thinh";
+	system("pause");
+	return 0;
+	}
